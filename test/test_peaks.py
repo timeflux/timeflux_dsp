90fdefde251ea5d67e70ff8246416d9bf67639db
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2019-05-06 16:00:21 +0200

    Update tests to handle the renaming of i/o ports

--- test/test_peaks.py
+++ test/test_peaks.py
@@ -2,13 +2,11 @@
 
 import pandas as pd
 import numpy as np
-from timeflux.core.registry import Registry
 import helpers
 
 from timeflux_dsp.nodes.peaks import RealTimeDetect, WindowDetect, Rate
 
-Registry.cycle_start = 0
-Registry.rate = 1
+
 
 
 def assert_dict_almost_equal(dict1, dict2, decimal=7):
@@ -27,12 +25,12 @@ def test_realtimepeak():
     data.reset()
     # mimic the scheduler
     output_peaks = []
-    output_peaks.append(node.o_events.data)
+    output_peaks.append(node.o.data)
     a = data.next(5).copy()
     while not a.empty:
         node.i.data = a.copy()
         node.update()
-        output_peaks.append(node.o_events.data)
+        output_peaks.append(node.o.data)
         a = data.next(5)
     cascade_output = pd.concat(output_peaks)
 
@@ -40,6 +38,7 @@ def test_realtimepeak():
     expected_labels = ['peak', 'valley']
     expected_data_peak = {'value': np.array([1.00546074]), 'lag': 0.03125, 'interval': 0.654236268}
     expected_data_valley = {'value': np.array([-1.01101112]), 'lag': 0.046875, 'interval': 0.654236268}
+
     pd.testing.assert_index_equal(expected_index, cascade_output.index)
     np.testing.assert_array_equal(expected_labels, cascade_output.label.values)
     assert_dict_almost_equal(expected_data_peak, cascade_output.data.values[0])
@@ -47,47 +46,25 @@ def test_realtimepeak():
 
 
 def test_windowpeak():
-    data = CustomData( data=data_ppg[:200])
+    data = helpers.CustomData( data=data_ppg[:200])
     node = WindowDetect(window=0.8, tol=0.5)
     data.reset()
     # mimic the scheduler
     output_peaks = []
-    output_peaks.append(node.o_events.data)
+    output_peaks.append(node.o.data)
     a = data.next(5).copy()
     while not a.empty:
         node.i.data = a.copy()
         node.update()
-        output_peaks.append(node.o_events.data)
+        output_peaks.append(node.o.data)
         a = data.next(5)
     cascade_output = pd.concat(output_peaks)
     expected_index = pd.DatetimeIndex(['2018-11-19 11:06:41.529004261', '2018-11-19 11:06:41.685242884'], dtype='datetime64[ns]', freq=None)
 
     expected_labels = ['peak', 'valley']
-    expected_data_peak = {'value': 0.7587511539459229, 'lag': 2.562340529, 'interval': 2.562340529}
+    expected_data_peak = {'value': 0.7587511539459229, 'lag': 0.0, 'interval': 2.562340529}
     expected_data_valley = {'value': -0.9906618595123292, 'lag': 2.718579152, 'interval': 2.718579152}
     pd.testing.assert_index_equal(expected_index, cascade_output.index)
     np.testing.assert_array_equal(expected_labels, cascade_output.label.values)
     assert_dict_almost_equal(expected_data_peak, cascade_output.data.values[0])
     assert_dict_almost_equal(expected_data_valley, cascade_output.data.values[1])
-
-
-def test_rate_window():
-    t0 = pd.Timestamp('2018-11-19 11:06:41.529004261')
-    expected_interval = 3
-    expected_rate = 1/expected_interval
-    t1 = t0+expected_interval*np.timedelta64(1,'s')
-    peak_event0 = pd.DataFrame(index=[t0], data = ['peak'], columns = ['label'])
-    peak_event1 = pd.DataFrame(index=[t1], data = ['peak'], columns = ['label'])
-
-
-    node_rate = Rate(window=0)
-    node_rate.i_events.data = peak_event0
-    node_rate.update()
-
-    assert node_rate._last == t0
-
-    node_rate.i_events.data = peak_event1
-    node_rate.update()
-
-    assert node_rate.o.data.rate.values[0]==expected_rate
-
