08cc6bd6dc857265d36ff39fc8892c62032c1aec
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2019-08-02 18:52:04 +0200

    Use of looper helper.

--- test/test_peaks.py
+++ test/test_peaks.py
@@ -4,19 +4,19 @@ import numpy as np
 import pandas as pd
 import pandas.util.testing as tm
 import pytest
-from timeflux.helpers.tests import CustomData
+from timeflux.helpers.testing import ReadData, Looper
 
 from timeflux_dsp.nodes.peaks import LocalDetect, RollingDetect
 
 
 @pytest.fixture(scope='module')
-def data_ppg():
+def ppg_generator():
     """Create object to mimic data streaming """
     # Signal of 300 points (sum of two sinus at 0.5 Hz  and 10 Hz) sampled at 50 kHz.
     df = pd.read_csv("../test/data/test_data_ppg.csv", index_col=None)
     df = pd.DataFrame(index=pd.to_datetime(df['index'].values), data=df["PPG"].values, columns=['PPG'])
-    data_ppg = CustomData(df[:200])
-    return data_ppg
+    ppg_generator = ReadData(df[:200])
+    return ppg_generator
 
 
 def assert_dict_almost_equal(dict1, dict2, decimal=7):
@@ -53,21 +53,15 @@ def test_localdetect_on_gaussians():
 
     original['A'] = gaussians_values
 
-    data_gaussians = CustomData(original)
+    data_gaussians = ReadData(original)
     node = LocalDetect(delta=0.5, tol=0.5)
-    # mimic the scheduler
-    output_peaks = []
-    output_peaks.append(node.o.data)
-    chunk = data_gaussians.next(5).copy()
-    while not chunk.empty:
-        node.i.data = chunk.copy()
-        node.update()
-        output_peaks.append(node.o.data)
-        chunk = data_gaussians.next(5)
-    cascade_output = pd.concat(output_peaks)
+    # loop across chunks
+    looper = Looper(data_gaussians, node)
+    cascade_output, _ = looper.run(chunk_size=5)
+
     pd.testing.assert_index_equal(peak_times,
                                   cascade_output[cascade_output.label == 'peak'].index)
-    cascade_peaks = pd.concat(
+    data_peaks = pd.concat(
         [pd.DataFrame(meta) for meta in cascade_output[cascade_output.label == 'peak'].data.values],
         ignore_index=True)
 
@@ -75,22 +69,16 @@ def test_localdetect_on_gaussians():
                                        value=[1.0, 1.0],
                                        interval=intervals,
                                        lag=lags))
-    pd.testing.assert_frame_equal(cascade_peaks, expected_peaks, check_like=True)
+    pd.testing.assert_frame_equal(data_peaks, expected_peaks, check_like=True)
 
 
-def test_localdetect_on_ppg(data_ppg):
-    node = LocalDetect(delta=0.1, tol=0.5)
-    data_ppg.reset()
-    # mimic the scheduler
-    output_peaks = []
-    output_peaks.append(node.o.data)
-    a = data_ppg.next(5).copy()
-    while not a.empty:
-        node.i.data = a.copy()
-        node.update()
-        output_peaks.append(node.o.data)
-        a = data_ppg.next(5)
-    cascade_output = pd.concat(output_peaks)
+def test_localdetect_on_ppg(ppg_generator):
+    node = LocalDetect(delta=0.5, tol=0.5)
+    # reset generator
+    ppg_generator.reset()
+    # loop across chunks
+    looper = Looper(ppg_generator, node)
+    cascade_output, _ = looper.run(chunk_size=5)
 
     expected_index = pd.DatetimeIndex(['2018-11-19 11:06:39.620900',
                                        '2018-11-19 11:06:39.794709043',
@@ -99,9 +87,13 @@ def test_localdetect_on_ppg(data_ppg):
                                        '2018-11-19 11:06:41.560254261',
                                        '2018-11-19 11:06:41.714533810'], dtype='datetime64[ns]', freq=None)
     expected_labels = ['peak', 'valley'] * 3
-    expected_data_peak = {'value': np.array([1.00546074]), 'lag': 0.03125, 'interval': 0.654236268,
+    expected_data_peak = {'value': np.array([1.00546074]),
+                          'lag': 0.064445281,
+                          'interval': 0.654236268,
                           'column_name': 'PPG'}
-    expected_data_valley = {'value': np.array([-1.01101112]), 'lag': 0.046875, 'interval': 0.654236268,
+    expected_data_valley = {'value': np.array([-1.01101112]),
+                            'lag': 0.437466566,
+                            'interval': 0.654236268,
                             'column_name': 'PPG'}
 
     pd.testing.assert_index_equal(expected_index, cascade_output.index)
@@ -110,25 +102,26 @@ def test_localdetect_on_ppg(data_ppg):
     assert_dict_almost_equal(expected_data_valley, cascade_output.data.values[1])
 
 
-def test_rollingdetect(data_ppg):
+def test_rollingdetect(ppg_generator):
     node = RollingDetect(window=0.8, tol=0.5)
-    data_ppg.reset()
-    # mimic the scheduler
-    output_peaks = []
-    output_peaks.append(node.o.data)
-    chunk = data_ppg.next(5).copy()
-    while not chunk.empty:
-        node.i.data = chunk.copy()
-        node.update()
-        output_peaks.append(node.o.data)
-        chunk = data_ppg.next(5)
-    cascade_output = pd.concat(output_peaks)
+
+    # reset generator
+    ppg_generator.reset()
+    # loop across chunks
+    looper = Looper(ppg_generator, node)
+    cascade_output, _ = looper.run(chunk_size=5)
+
     expected_index = pd.DatetimeIndex(['2018-11-19 11:06:41.529004261', '2018-11-19 11:06:41.685242884'],
                                       dtype='datetime64[ns]', freq=None)
 
     expected_labels = ['peak', 'valley']
-    expected_data_peak = {'value': 0.7587511539459229, 'lag': 0.0, 'interval': 2.562340529, 'column_name': 'PPG'}
-    expected_data_valley = {'value': -0.9906618595123292, 'lag': 2.718579152, 'interval': 2.718579152,
+    expected_data_peak = {'value': 0.7587511539459229,
+                          'lag': 0.0,
+                          'interval': 2.562340529,
+                          'column_name': 'PPG'}
+    expected_data_valley = {'value': -0.9906618595123292,
+                            'lag': 2.718579152,
+                            'interval': 2.718579152,
                             'column_name': 'PPG'}
     pd.testing.assert_index_equal(expected_index, cascade_output.index)
     np.testing.assert_array_equal(expected_labels, cascade_output.label.values)
