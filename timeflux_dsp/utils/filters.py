c0dcd8637cbbaca9a59e898875889934abf61100
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2019-08-02 18:57:54 +0200

    Renaming.

--- timeflux_dsp/utils/filters.py
+++ timeflux_dsp/utils/filters.py
@@ -4,6 +4,7 @@ import logging
 
 LOGGER = logging.getLogger('timeflux.' + __name__)
 
+
 def _get_com_factor(com, span, halflife, alpha):
     valid_count = sum(1 for _ in filter(None.__ne__, [com, span, halflife, alpha]))
     if valid_count > 1:
@@ -30,24 +31,26 @@ def _get_com_factor(com, span, halflife, alpha):
 
     return float(alpha)
 
-def _hz_to_nyq(freqs, nyq):
-    if type(freqs)==list:
-        return [freq/nyq for freq in freqs]
+
+def _hz_to_nyq(frequencies, nyq):
+    if isinstance(frequencies, list):
+        return [freq / nyq for freq in frequencies]
     else:
-        return freqs/nyq
+        return frequencies / nyq
 
-def _nyq_to_hz(freqs, nyq):
-    if type(freqs)==list:
-        return [freq*nyq for freq in freqs]
+
+def _nyq_to_hz(frequencies, nyq):
+    if isinstance(frequencies, list):
+        return [freq * nyq for freq in frequencies]
     else:
-        return freqs*nyq
+        return frequencies * nyq
 
 
-def design_edges(freqs, nyq, mode):
+def design_edges(frequencies, nyq, mode):
     """Design filter edges.
 
     Args:
-        freqs (list): Transition frequencies in Hz.
+        frequencies (list): Transition frequencies in Hz.
         mode (str): Filter mode (`lowpass`, `highpass`, `bandstop` or `bandpass`).
         nyq: Nyquist frequency (half sampling rate).
 
@@ -66,7 +69,7 @@ def design_edges(freqs, nyq, mode):
         * ``l_trans_bandwidth`` =
           .. math::  min(max(l_{freq} * 0.25, 2), l_{freq})
         * ``h_trans_bandwidth`` =
-          .. math:: min(max(h_{freq} * 0.25, 2.), fs / 2. - h_{freq})
+          .. math:: min(max(h_{freq} * 0.25, 2.), rate / 2. - h_{freq})
 
     **Band-pass filter**
 
@@ -172,83 +175,83 @@ def design_edges(freqs, nyq, mode):
     """
 
     # normalize frequencies
-    _normalized_freqs = [f / nyq for f in freqs]
+    _normalized_freqs = [f / nyq for f in frequencies]
 
     if mode == 'highpass':
-        if len(freqs) == 1:
-            l_freq = freqs[0]
+        if len(frequencies) == 1:
+            l_freq = frequencies[0]
             l_trans_bandwidth = min(max(l_freq * 0.25, 2), l_freq)
-            freqs = [l_freq - l_trans_bandwidth / 2, l_freq + l_trans_bandwidth / 2]
+            frequencies = [l_freq - l_trans_bandwidth / 2, l_freq + l_trans_bandwidth / 2]
             LOGGER.info("Filter Design: assuming {tb} Hz transition band.".format(
-                tb=freqs[1] - freqs[0]))
-        wp, ws = freqs[1], freqs[0]
+                tb=frequencies[1] - frequencies[0]))
+        wp, ws = frequencies[1], frequencies[0]
 
     elif mode == 'lowpass':
 
-        if len(freqs) == 1:
-            h_freq = freqs[0]
+        if len(frequencies) == 1:
+            h_freq = frequencies[0]
             h_trans_bandwidth = min(max(h_freq * 0.25, 2.), nyq - h_freq)
-            freqs = [h_freq-h_trans_bandwidth/2, h_freq + h_trans_bandwidth/2]
+            frequencies = [h_freq - h_trans_bandwidth / 2, h_freq + h_trans_bandwidth / 2]
             LOGGER.info(
-                "IIRFilter: assuming {tb} Hz transition band.".format(tb=freqs[1] - freqs[0]))
-        wp, ws = _hz_to_nyq(freqs, nyq)
-
+                "Filter design: assuming {tb} Hz transition band.".format(tb=frequencies[1] - frequencies[0]))
+        wp, ws = _hz_to_nyq(frequencies, nyq)
 
     elif mode == "bandpass":
-        if len(freqs) == 2:
-            lofreq, hifreq = freqs
+        if len(frequencies) == 2:
+            lofreq, hifreq = frequencies
             minfreq = lofreq - 1 if lofreq > 1 else lofreq / 2
             maxfreq = (hifreq + 1 if hifreq < nyq - 1 else (hifreq + nyq) / 2)
-            freqs = [minfreq, lofreq, hifreq, maxfreq]
+            frequencies = [minfreq, lofreq, hifreq, maxfreq]
             LOGGER.info("Filter Design: assuming {tb1} and {tb2} Hz transition band.".format(
-                tb1=freqs[2] - freqs[1],
-                tb2=freqs[3] - freqs[0]))
-        wp, ws = _hz_to_nyq([freqs[1], freqs[2]], nyq), _hz_to_nyq([freqs[0], freqs[3]], nyq)
+                tb1=frequencies[2] - frequencies[1],
+                tb2=frequencies[3] - frequencies[0]))
+        wp, ws = _hz_to_nyq([frequencies[1], frequencies[2]], nyq), _hz_to_nyq([frequencies[0], frequencies[3]], nyq)
 
     elif mode == "bandstop":
-        if len(freqs) == 2:
-            l_freq, h_freq = freqs
+        if len(frequencies) == 2:
+            l_freq, h_freq = frequencies
             l_trans_bandwidth = min(max(l_freq * 0.25, 2), l_freq)
             h_trans_bandwidth = min(max(h_freq * 0.25, 2.), nyq - h_freq)
 
-            freqs = [l_freq - l_trans_bandwidth / 2, l_freq + l_trans_bandwidth / 2, h_freq-h_trans_bandwidth/2, h_freq + h_trans_bandwidth/2]
+            frequencies = [l_freq - l_trans_bandwidth / 2, l_freq + l_trans_bandwidth / 2,
+                           h_freq - h_trans_bandwidth / 2, h_freq + h_trans_bandwidth / 2]
 
             LOGGER.info("Filter Design: assuming {tb1} and {tb2} Hz transition band.".format(
-                tb1=freqs[3] - freqs[0],
-                tb2=freqs[1] - freqs[2]))
-        wp, ws = _hz_to_nyq([freqs[0], freqs[3]], nyq), _hz_to_nyq([freqs[1], freqs[2]], nyq)
+                tb1=frequencies[3] - frequencies[0],
+                tb2=frequencies[1] - frequencies[2]))
+        wp, ws = _hz_to_nyq([frequencies[0], frequencies[3]], nyq), _hz_to_nyq([frequencies[1], frequencies[2]], nyq)
 
     else:
         raise ValueError("Unknown filter mode given: {mode} ".format(mode=mode))
 
     if mode == 'bandpass':
-        freqs = [0] + freqs + [nyq]
+        frequencies = [0] + frequencies + [nyq]
         gains = [0, 0, 1, 1, 0, 0]
     elif mode == 'bandstop':
-        freqs = [0] + freqs + [nyq]
+        frequencies = [0] + frequencies + [nyq]
         gains = [1, 1, 0, 0, 1, 1]
     elif mode == 'highpass':
-        freqs = [0] + freqs + [nyq]
+        frequencies = [0] + frequencies + [nyq]
         gains = [0, 0, 1, 1]
     elif mode == 'lowpass':
-        freqs = [0] + freqs + [nyq]
+        frequencies = [0] + frequencies + [nyq]
         gains = [1, 1, 0, 0]
     else:
         raise ValueError("unsupported filter mode: {mode}".format(mode=mode))
 
-    if any([f > nyq for f in freqs]):
+    if any([f > nyq for f in frequencies]):
         raise ValueError("One of the given frequencies exceeds the "
                          "Nyquist frequency of the signal (%.1f)." % nyq)
 
-    return freqs, gains, wp, ws
+    return frequencies, gains, wp, ws
 
 
-def _filter_attenuation(h, freqs, gains):
+def _filter_attenuation(h, frequencies, gains):
     """ Compute minimum attenuation at stop frequency.
 
      Args:
         h (array): Filter coefficients.
-        freqs (list): Transition frequencies normalized.
+        frequencies (list): Transition frequencies normalized.
         gains (array):  Filter gain at frequency sampling points.
 
      Returns:
@@ -260,24 +263,24 @@ def _filter_attenuation(h, freqs, gains):
         Adapted from mne.filters
 
     """
-    freqs = np.array(freqs)
+    frequencies = np.array(frequencies)
     from scipy.signal import freqz
-    _, filt_resp = freqz(h.ravel(), worN=np.pi * freqs)
+    _, filt_resp = freqz(h.ravel(), worN=np.pi * frequencies)
     filt_resp = np.abs(filt_resp)  # use amplitude response
     filt_resp[np.where(gains == 1)] = 0
     idx = np.argmax(filt_resp)
     att_db = -20 * np.log10(np.maximum(filt_resp[idx], 1e-20))
-    att_freq = freqs[idx]
+    att_freq = frequencies[idx]
     return att_db, att_freq
 
-def construct_fir_filter(fs, freqs, gains, order, phase, window, design):
+
+def construct_fir_filter(rate, frequencies, gains, order, phase, window, design):
     """ Construct coeffs of FIR filter.
 
     Args:
-        fs (float): Nominal sampling rate of the input data.
+        rate (float): Nominal sampling rate of the input data.
         order (int): Filter order
-        freqs (list): Transition frequencies in Hz.
-        mode (str): Filter mode (`lowpass`, `highpass`, `bandstop` or `bandpass`).
+        frequencies (list): Transition frequencies in Hz.
         design (str|'firwin2'): Design of the transfert function of the filter.
         phase (str|`linear`): Phase response ("zero", "zero-double" or "minimum").
         window (float|`hamming`): The window to use in FIR design, ("hamming", "hann", or "blackman").
@@ -295,7 +298,7 @@ def construct_fir_filter(fs, freqs, gains, order, phase, window, design):
 
 
     """
-    nyq = fs/2.
+    nyq = rate / 2.
     if design == 'firwin2':
         from scipy.signal import firwin2 as design
     else:
@@ -305,55 +308,53 @@ def construct_fir_filter(fs, freqs, gains, order, phase, window, design):
     # issue a warning if attenuation is less than this
     min_att_db = 12 if phase == 'minimum' else 20
 
-    if freqs[0] != 0 or freqs[-1] != nyq:
+    if frequencies[0] != 0 or frequencies[-1] != nyq:
         raise ValueError('freq must start at 0 and end an Nyquist (%s), got %s'
-                         % (nyq, freqs))
+                         % (nyq, frequencies))
     gains = np.array(gains)
 
     if window == "kaiser":
-        diffs = np.diff(freqs)
+        diffs = np.diff(frequencies)
         width = min(diffs[diffs > 0])
-        beta = signal.kaiser_beta(signal.kaiser_atten(order, width/nyq))
+        beta = signal.kaiser_beta(signal.kaiser_atten(order, width / nyq))
         window = ("kaiser", beta)
-
 
     # check zero phase length
     N = int(order)
-    if  N % 2 == 0:
+    if N % 2 == 0:
         if phase == 'zero':
             LOGGER.info('filter_length must be odd if phase="zero", '
-                               'got %s' % N )
-            N+=1
+                        'got %s' % N)
+            N += 1
         elif phase == 'zero-double' and gains[-1] == 1:
             N += 1
     # construct symmetric (linear phase) filter
     if phase == 'minimum':
-        h = design(N * 2 - 1, freqs, gains, fs=fs, window=window)
+        h = design(N * 2 - 1, frequencies, gains, fs=rate, window=window)
         h = signal.minimum_phase(h)
     else:
-        h = design(N, freqs, gains, fs=fs, window=window)
+        h = design(N, frequencies, gains, fs=rate, window=window)
     assert h.size == N
-    att_db, att_freq = _filter_attenuation(h, freqs, gains)
+    att_db, att_freq = _filter_attenuation(h, frequencies, gains)
     if phase == 'zero-double':
         att_db += 6
     if att_db < min_att_db:
-        att_freq *= fs / 2.
+        att_freq *= rate / 2.
         LOGGER.info('Attenuation at stop frequency %0.1fHz is only %0.1fdB. '
-             'Increase filter_length for higher attenuation.'
-             % (att_freq, att_db))
+                    'Increase filter_length for higher attenuation.'
+                    % (att_freq, att_db))
     return h
 
 
-
-def construct_iir_filter(fs, freqs, mode, order=None, design="butter", pass_loss=3.0, stop_atten=50.0, output="sos"):
-
+def construct_iir_filter(rate, frequencies, filter_type, order=None, design="butter",
+                         pass_loss=3.0, stop_atten=50.0, output="sos"):
     """Calculate an IIR filter kernel for a given sampling rate.
 
     Args:
-        fs (float): Nominal sampling rate of the input data.
+        rate (float): Nominal sampling rate of the input data.
         order (int): Filter order
-        freqs (list): Transition frequencies in Hz.
-        mode (str): Filter mode (`lowpass`, `highpass`, `bandstop` or `bandpass`).
+        frequencies (list): Transition frequencies in Hz.
+        filter_type (str): Filter mode (`lowpass`, `highpass`, `bandstop` or `bandpass`).
         design (str|'butter'): Design of the transfert function of the filter (`butter`, `cheby1`, `cheby2`, `ellip`, `bessel`)
         pass_loss (float|`3.0`): For Chebyshev and elliptic filters, provides the maximum ripple in the passband. (dB).
         stop_atten (float|`50.0`): For Chebyshev and elliptic filters, provides the minimum attenuation in the stop band. (dB)
@@ -374,29 +375,20 @@ def construct_iir_filter(fs, freqs, mode, order=None, design="butter", pass_loss
 
     _minorder = {"butter": signal.buttord, "cheby1": signal.cheb1ord, "cheby2": signal.cheb2ord, "ellip": signal.ellip}
 
-    nyq = fs / 2.0
+    nyq = rate / 2.0
 
     if order:
         # use order-based design
-        wn = _hz_to_nyq(freqs, nyq)
+        wn = _hz_to_nyq(frequencies, nyq)
         out = signal.iirfilter(N=order, Wn=wn,
                                rp=pass_loss,
                                rs=stop_atten,
-                               btype=mode, ftype=design,
+                               btype=filter_type, ftype=design,
                                output=output)
-        # sos = signal.iirfilter(N=order, Wn=wn,
-        #                         rp=pass_loss,
-        #                         rs=stop_atten,
-        #                         btype=mode, ftype=design,
-        #                         output='sos')
-        return out, freqs
+        return out, frequencies
     else:
-        freqs, gains, wp, ws = design_edges(freqs, nyq, mode)
+        frequencies, gains, wp, ws = design_edges(frequencies, nyq, filter_type)
         out = signal.iirdesign(wp=wp, ws=ws, gstop=stop_atten,
                                gpass=pass_loss,
                                ftype=design, output=output)
-        # sos = signal.iirdesign(wp=wp, ws=ws, gstop=stop_atten,
-        #                        gpass=pass_loss,
-        #                        ftype=design, output='sos')
-        return out, freqs
-
+        return out, frequencies
